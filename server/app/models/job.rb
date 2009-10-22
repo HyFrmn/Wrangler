91efce737135e9c908ba2f30b4783fa3bc28e4f4
Petersen <mpetersen@flexo.lite3d.com>
2009-10-22 14:45:08 -0500

    Cleaned up job view and model for webserver.

--- server/app/models/job.rb
+++ server/app/models/job.rb
@@ -22,10 +22,31 @@ class Job < ActiveRecord::Base
   end
 
   def runtime
-    TaskLog.sum(:time, :joins => :task, :conditions => ['tasks.job_id = ?', self[:id]])
+    TaskLog.sum(:time, :joins => :task, :conditions => ['tasks.job_id = ? AND task_logs.time > 0', self[:id]]).to_f
   end
 
   def averagetime
-    TaskLog.average(:time, :joins => :task, :conditions => ['tasks.job_id = ? and task_logs.time > 0', self[:id]])
+    TaskLog.average(:time, :joins => :task, :conditions => ['tasks.job_id = ? and task_logs.time > 0', self[:id]]).to_f
   end
+  
+  def task_count
+    Task.count(:conditions => ["job_id = ?", self[:id]]).to_f
+  end
+  
+  def finished
+    Task.count(:conditions => ["job_id = ? AND status = 3", self[:id]]).to_f
+  end
+
+  def queued
+    Task.count(:conditions => ["job_id = ? AND status = 1", self[:id]]).to_f
+  end
+
+  def estimate
+    self.queued * self.averagetime
+  end
+
+  def progress
+      (self.finished / self.task_count) * 100
+  end
+  
 end
