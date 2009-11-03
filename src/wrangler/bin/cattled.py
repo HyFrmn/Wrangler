4dc26f8047e0f9b52eab910cd44c632b0bfb2a0f
Petersen <mpetersen@flexo.lite3d.com>
2009-11-03 15:20:37 -0600

    Added automatic wrangler module detection to daemons scripts.

--- src/wrangler/bin/cattled.py
+++ src/wrangler/bin/cattled.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+wrangler_module_dir = os.path.abspath(__file__)
+wrangler_module_dir = os.path.dirname(wrangler_module_dir)
+wrangler_module_dir = os.path.dirname(wrangler_module_dir)
+wrangler_module_dir = os.path.dirname(wrangler_module_dir)
+
+sys.path.append(wrangler_module_dir)
+
 from wrangler.hardware import info
 from wrangler.cattled.server import CattleServer
 
